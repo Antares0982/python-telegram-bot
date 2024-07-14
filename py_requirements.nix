f2e0c176c638af3d8609d69e62660835a2553e36
antares <antares@alyr.dev>
2024-07-14 15:47:30 +0800

    Modify pyproject.toml to publish customized fork

---
+++ py_requirements.nix
@@ -0,0 +1,19 @@
+pkgs: pypkgs: with pypkgs;
+[
+  # pre-commit
+  setuptools
+  pytest
+  pytest-asyncio
+  pytest-xdist
+  flaky
+  beautifulsoup4
+  wheel
+  httpx
+  cryptography
+  aiolimiter
+  tornado
+  cachetools
+  APScheduler
+  pytz
+  # add packages here
+]
